f51e7196a62713213a0a60a2b0c2ac4ed78995b3
Michael Enion <caewok_2000@yahoo.com>
2022-06-14 16:52:02 -0700

    Add module skeleton

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,21 @@
-Hooks.once('init', async function() {
+/* globals
+Hooks
+*/
+"use strict";
 
+import { MODULE_ID } from "./const.js";
+import { log } from "./util.js";
+
+
+Hooks.once("init", async function() {
+  log("Hello!");
 });
 
-Hooks.once('ready', async function() {
-
+/**
+ * Tell DevMode that we want a flag for debugging this module.
+ * https://github.com/League-of-Foundry-Developers/foundryvtt-devMode
+ */
+Hooks.once("devModeReady", ({ registerPackageDebugFlag }) => {
+  registerPackageDebugFlag(MODULE_ID);
 });
+
---
+++ scripts/util.js
@@ -0,0 +1,21 @@
+/* globals
+game
+*/
+"use strict";
+
+import { MODULE_ID } from "./const.js";
+
+/**
+ * Log message only when debug flag is enabled from DevMode module.
+ * @param {Object[]} args  Arguments passed to console.log.
+ */
+export function log(...args) {
+  try {
+    const isDebugging = game.modules.get("_dev-mode")?.api?.getPackageDebugValue(MODULE_ID);
+    if ( isDebugging ) {
+      console.log(MODULE_ID, "|", ...args);
+    }
+  } catch(e) {
+    // Empty
+  }
+}
