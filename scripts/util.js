163dd011b7147a9fca196ecf7c7dc241bcb036e8
Michael Enion <caewok_2000@yahoo.com>
2022-07-15 19:45:44 -0700

    Move orient2dPixelLine to util.js

--- scripts/util.js
+++ scripts/util.js
@@ -19,3 +19,26 @@ export function log(...args) {
     // Empty
   }
 }
+
+
+/**
+ * Is point c counterclockwise, clockwise, or colinear w/r/t ray with endpoints A|B?
+ * If the point is within ± √2 / 2 of the line, it will be considered collinear.
+ * See equivalentPixel function for further discussion on the choice of √2 / 2.
+ * @param {Point} a   First endpoint of the segment
+ * @param {Point} b   Second endpoint of the segment
+ * @param {Point} c   Point to test
+ * @return {number}   Same as foundry.utils.orient2dFast
+ *                    except 0 if within √2 /2 of the ray.
+ *                    Positive: c counterclockwise/left of A|B
+ *                    Negative: c clockwise/right of A|B
+ *                    Zero: A|B|C collinear.
+ */
+export function orient2dPixelLine(a, b, c) {
+  const orientation = foundry.utils.orient2dFast(a, b, c);
+  const dist2 = Math.pow(b.x - a.x, 2) + Math.pow(b.y - a.y, 2);
+  const orientation2 = Math.pow(orientation, 2);
+  const cutoff = 0.5 * dist2; // 0.5 is (√2 / 2)^2.
+
+  return (orientation2 < cutoff) ? 0 : orientation;
+}
