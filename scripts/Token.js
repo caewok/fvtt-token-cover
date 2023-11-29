b4db3976a89e04b3537cca25e47863e766122929
Michael Enion <caewok_2000@yahoo.com>
2023-11-29 14:45:13 -0800

    Remove token patches duplicated in LOS/Token

--- scripts/Token.js
+++ scripts/Token.js
@@ -1,13 +1,10 @@
 /* globals
-canvas,
-game,
-PIXI
+game
 */
 /* eslint no-unused-vars: ["error", { "argsIgnorePattern": "^_" }] */
 
 // Patches for the Token class
 
-import { ConstrainedTokenBorder } from "./LOS/ConstrainedTokenBorder.js";
 import { MODULE_ID, MODULES_ACTIVE, COVER, IGNORES_COVER_HANDLER } from "./const.js";
 import { CoverCalculator } from "./CoverCalculator.js";
 import { SETTINGS, Settings } from "./Settings.js";
@@ -114,27 +111,6 @@ PATCHES.BASIC.WRAPS = {
 // ----- NOTE: Getters ----- //
 
 /**
- * New getter: Token.prototype.constrainedTokenBorder
- * Determine the constrained border shape for this token.
- * @returns {ConstrainedTokenShape|PIXI.Rectangle}
- */
-function constrainedTokenBorder() { return ConstrainedTokenBorder.get(this).constrainedBorder(); }
-
-/**
- * New getter: Token.prototype.tokenBorder
- * Determine the correct border shape for this token. Utilize the cached token shape.
- * @returns {PIXI.Polygon|PIXI.Rectangle}
- */
-function tokenBorder() { return this.tokenShape.translate(this.x, this.y); }
-
-/**
- * New getter: Token.prototype.tokenShape
- * Cache the token shape.
- * @type {PIXI.Polygon|PIXI.Rectangle}
- */
-function tokenShape() { return this._tokenShape || (this._tokenShape = calculateTokenShape(this)); }
-
-/**
  * New getter: Token.prototype.coverType
  * Determine what type of cover the token has, if any.
  * @type {COVER_TYPES}
@@ -159,31 +135,11 @@ function ignoresCoverType() {
 }
 
 PATCHES.BASIC.GETTERS = {
-  constrainedTokenBorder,
-  tokenBorder,
-  tokenShape,
   coverType,
   ignoresCoverType
 };
 
-
 // ----- NOTE: Helper functions ----- //
-/**
- * Theoretical token shape at 0,0 origin.
- * @returns {PIXI.Polygon|PIXI.Rectangle}
- */
-function calculateTokenShape(token) {
-  // TODO: Use RegularPolygon shapes for use with WeilerAtherton
-  // Hexagon (for width .5 or 1)
-  // Square (for width === height)
-  let shape;
-  if ( canvas.grid.isHex ) {
-    const pts = canvas.grid.grid.getBorderPolygon(token.document.width, token.document.height, 0);
-    if ( pts ) shape = new PIXI.Polygon(pts);
-  }
-
-  return shape || new PIXI.Rectangle(0, 0, token.w, token.h);
-}
 
 /**
  * Determine if the user's token is the current combatant in the active tracker.
