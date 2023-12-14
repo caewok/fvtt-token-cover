d59e41545d3ba8d59b7d95e1691595686c81d273
Michael Enion <caewok_2000@yahoo.com>
2023-12-14 08:30:00 -0800

    Add ModuleSettingsAbstract class

--- scripts/settings.js
+++ scripts/settings.js
@@ -8,6 +8,7 @@ game
 /* eslint no-unused-vars: ["error", { "argsIgnorePattern": "^_" }] */
 "use strict";
 
+import { ModuleSettingsAbstract } from "./ModuleSettingsAbstract.js";
 import { MODULE_ID, MODULES_ACTIVE, COVER } from "./const.js";
 import { STATUS_EFFECTS } from "./status_effects.js";
 import { SettingsSubmenu } from "./SettingsSubmenu.js";
@@ -16,23 +17,6 @@ import {
   LowCoverEffectConfig,
   MediumCoverEffectConfig,
   HighCoverEffectConfig } from "./EnhancedEffectConfig.js";
-
-// Patches for the Setting class
-export const PATCHES = {};
-PATCHES.BASIC = {};
-
-// ----- NOTE: Hooks ----- //
-
-/**
- * Wipe the settings cache on update
- */
-function updateSetting(document, change, options, userId) {  // eslint-disable-line no-unused-vars
-  const [module, ...arr] = document.key.split(".");
-  const key = arr.join("."); // If the key has periods, multiple will be returned by split.
-  if ( module === MODULE_ID && Settings.cache.has(key) ) Settings.cache.delete(key);
-}
-
-PATCHES.BASIC.HOOKS = { updateSetting };
 
 export const SETTINGS = {
   SUBMENU: "submenu",
@@ -142,9 +126,7 @@ export const SETTINGS = {
   }
 };
 
-export class Settings {
-  /** @type {Map<string, *>} */
-  static cache = new Map();
+export class Settings extends ModuleSettingsAbstract {
 
   /** @type {object} */
   static KEYS = SETTINGS;
@@ -271,62 +253,12 @@ export class Settings {
     else CONFIG.statusEffects[currIdx] = coverEffect;
   }
 
-  // ---- NOTE: Settings static methods ---- //
-
-  /**
-   * Retrive a specific setting.
-   * Cache the setting.  For caching to work, need to clean the cache whenever a setting below changes.
-   * @param {string} key
-   * @returns {*}
-   */
-  static get(key) {
-    const cached = this.cache.get(key);
-    if ( typeof cached !== "undefined" ) {
-      const origValue = game.settings.get(MODULE_ID, key);
-      if ( origValue !== cached ) {
-        console.debug(`Settings cache fail: ${origValue} !== ${cached} for key ${key}`);
-        return origValue;
-      }
-
-      return cached;
-
-    }
-    const value = game.settings.get(MODULE_ID, key);
-    this.cache.set(key, value);
-    return value;
-  }
-
-  /**
-   * Set a specific setting.
-   * @param {string} key
-   * @param {*} value
-   * @returns {Promise<boolean>}
-   */
-  static async set(key, value) {
-    this.cache.delete(key);
-    return game.settings.set(MODULE_ID, key, value);
-  }
-
-  /**
-   * Register a specific setting.
-   * @param {string} key        Passed to registerMenu
-   * @param {object} options    Passed to registerMenu
-   */
-  static register(key, options) { game.settings.register(MODULE_ID, key, options); }
-
-  /**
-   * Register a submenu.
-   * @param {string} key        Passed to registerMenu
-   * @param {object} options    Passed to registerMenu
-   */
-  static registerMenu(key, options) { game.settings.registerMenu(MODULE_ID, key, options); }
-
   /**
    * Register all settings
    */
   static registerAll() {
     const { KEYS, register, registerMenu } = this;
-    const localize = key => game.i18n.localize(`${MODULE_ID}.settings.${key}`);
+    const localize = this.localize;
     const PT_TYPES = KEYS.POINT_TYPES;
     const RTYPES = [PT_TYPES.CENTER, PT_TYPES.FIVE, PT_TYPES.NINE];
     const PT_OPTS = KEYS.LOS.TARGET.POINT_OPTIONS;
