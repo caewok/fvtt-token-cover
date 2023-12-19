eaa0c2c4dcd6a81331bfd874860f869f4d558f75
Michael Enion <caewok_2000@yahoo.com>
2023-12-18 19:11:59 -0800

    Update changelog

--- scripts/changelog.js
+++ scripts/changelog.js
@@ -74,7 +74,13 @@ Hooks.once("ready", () => {
           To simplify the module and improve debugging, I have split out token cover from
           token visibility. There are now two modules: [Alternative Token Visibility](https://github.com/caewok/fvtt-token-visibility) and
           [Alternative Token Cover](https://github.com/caewok/fvtt-token-cover). You can use one module without
-          the other, or both in combination.`
+          the other, or both in combination.
+
+          - **Refactored Settings:** New settings configuration submenu and reorganized settings. Sorry, I did not attempt
+            to transfer over your old settings!
+          - **Revised Area3d Algorithm:** Area3d and Points algorithms both now handle handle transparent tiles. Area3d will
+            automatically switch to webGL to render a 3d view with tiles when necessary to determine cover.
+          `
     })
 
     .build()
