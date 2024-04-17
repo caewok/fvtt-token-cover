4682defd9e3d55ffb032ee2296bdb2625bea6e68
Michael Enion <caewok_2000@yahoo.com>
2024-04-17 16:27:48 -0700

    🎸 feat|Debug|Add back in debug hooks
    Temporarily took out the debug hooks to simplify Token hooks.
    
    Added back now that Cover Effects basically work.

--- scripts/Token.js
+++ scripts/Token.js
@@ -55,6 +55,116 @@ Triggers:
 - Token is moved. Wipe existing cover calculations. Refresh based on control or target.
 */
 
+// ----- NOTE: Debug Hooks ----- //
+
+/**
+ * Hook: controlToken
+ * If the token is uncontrolled, clear debug drawings.
+ * @event controlObject
+ * @category PlaceableObject
+ * @param {PlaceableObject} object The object instance which is selected/deselected.
+ * @param {boolean} controlled     Whether the PlaceableObject is selected or not.
+ */
+async function controlTokenDebugHook(token, controlled) {
+  if ( !token[MODULE_ID].coverCalc ) return;
+  const coverCalc = token.coverCalculator;
+  coverCalc.clearDebug();
+  if ( controlled ) {
+    if ( coverCalc.calc.openDebugPopout ) await coverCalc.calc.openDebugPopout();
+    updateDebugForControlledToken(token);
+  }
+}
+
+/**
+ * Hook: targetToken
+ * Check for other controlled tokens and update their Area3d debug popout to point at this target.
+ * @param {User} user        The User doing the targeting
+ * @param {Token} token      The targeted Token
+ * @param {boolean} targeted Whether the Token has been targeted or untargeted
+ */
+function targetTokenDebugHook(user, target, targeted) {
+  if ( !targeted || game.user !== user ) return;
+  canvas.tokens.placeables
+    .filter(t => t !== target && t.controlled && t[MODULE_ID]?.coverCalc)
+    .forEach(t => {
+      const coverCalc = t.coverCalculator;
+      if ( !coverCalc._draw3dDebug ) return;
+      coverCalc.calc._clearCache();
+      coverCalc.target = target;
+      coverCalc.calc.updateDebug();
+    });
+
+}
+
+/**
+ * Hook: updateToken
+ * If the token moves, clear all debug drawings.
+ * @param {Document} tokenD                         The existing Document which was updated
+ * @param {object} change                           Differential data that was used to update the document
+ * @param {DocumentModificationContext} options     Additional options which modified the update request
+ * @param {string} userId                           The ID of the User who triggered the update workflow
+ */
+function updateTokenDebugHook(tokenD, change, _options, _userId) {
+  if ( !(Object.hasOwn(change, "x")
+      || Object.hasOwn(change, "y")
+      || Object.hasOwn(change, "elevation")
+      || Object.hasOwn(change, "rotation")) ) return;
+
+  // Token moved
+  const token = tokenD.object;
+  if ( token.controlled ) updateDebugForControlledToken(token);
+  updateDebugForRelatedTokens(token);
+}
+
+/**
+ * If token position is refreshed (i.e., clone), then clear debug.
+ * @param {PlaceableObject} object    The object instance being refreshed
+ * @param {RenderFlag} flags
+ */
+function refreshTokenDebugHook(token, flags) {
+  if ( !flags.refreshPosition ) return;
+  if ( token.controlled ) updateDebugForControlledToken(token);
+  updateDebugForRelatedTokens(token);
+}
+
+function updateDebugForControlledToken(changedToken) {
+  // If this token is controlled, update its LOS canvas display to every other token.
+  const changedCalc = changedToken[MODULE_ID]?.coverCalc.calc;
+  if ( !changedCalc ) return;
+  changedCalc.clearDebug();
+  canvas.tokens.placeables.forEach(token => {
+    if ( token === changedToken ) return;
+    changedCalc._clearCache();
+    changedCalc.target = token;
+    changedCalc.updateDebug();
+  });
+
+}
+
+/**
+ * Update debug graphics for tokens related to this one.
+ * @param {Token} changedToken    Token that has been updated (position, etc.)
+ */
+function updateDebugForRelatedTokens(changedToken) {
+  // For any other controlled token, update its LOS canvas display for this one.
+  canvas.tokens.placeables
+    .filter(t => t !== changedToken && t.controlled && t[MODULE_ID]?.coverCalc)
+    .forEach(token => {
+      const coverCalc = token.coverCalculator;
+      if ( coverCalc.target === changedToken ) coverCalc.clearDebug();
+      coverCalc.calc._clearCache();
+      coverCalc.target = changedToken;
+      coverCalc.calc.updateDebug();
+    });
+}
+
+PATCHES.DEBUG.HOOKS = {
+  controlToken: controlTokenDebugHook,
+  updateToken: updateTokenDebugHook,
+  refreshToken: refreshTokenDebugHook,
+  targetToken: targetTokenDebugHook
+};
+
 
 // ----- NOTE: Hooks ----- //
 
