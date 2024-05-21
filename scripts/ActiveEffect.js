97cc852d15a2a666c2dea3e56277154f6c941ab6
Michael Enion <caewok_2000@yahoo.com>
2024-05-21 14:11:27 -0700

    🐛 fix|CoverFlag|Update cover rule flags in settings when AE updated

--- scripts/ActiveEffect.js
+++ scripts/ActiveEffect.js
@@ -1,10 +1,13 @@
 /* globals
-Actor
+Actor,
+foundry,
+isEmpty
 */
 /* eslint no-unused-vars: ["error", { "argsIgnorePattern": "^_" }] */
 "use strict";
 
 import { FLAGS, MODULE_ID } from "./const.js";
+import { Settings } from "./settings.js";
 
 // Patches for the ActiveEffect class
 export const PATCHES = {};
@@ -30,6 +33,31 @@ function createActiveEffect(document, _options, _userId) {
 }
 
 /**
+ * When updating an active effect, store the changed cover rule flags in cover flag settings.
+ * @param {Document} document                       The existing Document which was updated
+ * @param {object} change                           Differential data that was used to update the document
+ * @param {DocumentModificationContext} options     Additional options which modified the update request
+ * @param {string} userId                           The ID of the User who triggered the update workflow
+ */
+function updateActiveEffect(document, change, _options, _userId) {
+  const modFlags = change?.flags?.[MODULE_ID];
+  if ( !modFlags ) return;
+  const ceId = modFlags[FLAGS.COVER_EFFECT.ID];
+  if ( !ceId || !modFlags[FLAGS.COVER_EFFECT.LOCAL] ) return;
+
+  const modFlagSet = new Set(Object.keys(modFlags));
+  const newSettings = {};
+  for( const flag in FLAGS.COVER_EFFECT.RULES ) {
+    if ( !modFlagSet.has(flag) ) continue;
+    newSettings[flag] = modFlags[flag];
+  }
+  if ( isEmpty(newSettings) ) return;
+  const prevSettings = Settings.get(Settings.KEYS.COVER_EFFECTS.RULES) ?? {};
+  foundry.utils.mergeObject(prevSettings, newSettings, { inplace: true });
+  Settings.set(Settings.KEYS.COVER_EFFECTS.RULES, prevSettings); // Async
+}
+
+/**
  * When adding an active effect, check for overriding effect.
  * @param {Document} document                       The new Document instance which has been created
  * @param {DocumentModificationContext} options     Additional options which modified the creation request
@@ -46,4 +74,4 @@ function deleteActiveEffect(document, _options, _userId) {
   token.tokencover.updateCover();
 }
 
-PATCHES.BASIC.HOOKS = { createActiveEffect, deleteActiveEffect };
+PATCHES.BASIC.HOOKS = { createActiveEffect, deleteActiveEffect, updateActiveEffect };
