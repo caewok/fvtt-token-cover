67fa6cbdd93b7c05ae6b9a37703f379cc7f95d07
Michael Enion <caewok_2000@yahoo.com>
2024-09-04 08:17:39 -0700

    🎸 feat|Templates|Display cover when hovering over template

--- scripts/MeasuredTemplate.js
+++ scripts/MeasuredTemplate.js
@@ -1,14 +1,9 @@
 /* globals
-canvas,
-CONFIG,
-foundry,
-game,
-MouseInteractionManager,
-PIXI,
-_token
 */
 /* eslint no-unused-vars: ["error", { "argsIgnorePattern": "^_" }] */
 "use strict";
+
+import { TokenCover } from "./TokenCover.js";
 
 export const PATCHES = {};
 PATCHES.TEMPLATES = {};
@@ -20,6 +15,21 @@ PATCHES.TEMPLATES = {};
  */
 function hoverMeasuredTemplate(template, hovering) {
   console.log(`hoverMeasuredTemplate|${hovering} for ${template.id}`);
+  if ( hovering ) {
+    // When hovering, the template is the only attacker.
+    TokenCover.clearAttackers(false);
+    TokenCover.addAttacker(template);
+
+  // When not hovering, remove this template as an attacker.
+  } else TokenCover.removeAttacker(template);
 }
 
-PATCHES.TEMPLATES.HOOKS = { hoverMeasuredTemplate };
+/**
+ * Hook destroyMeasuredTemplate to remove the template from the attackers
+ * @param {MeasuredTemplate} template    The object instance being destroyed
+ */
+function destroyMeasuredTemplate(template) {
+  TokenCover.removeAttacker(template);
+}
+
+PATCHES.TEMPLATES.HOOKS = { hoverMeasuredTemplate, destroyMeasuredTemplate };
