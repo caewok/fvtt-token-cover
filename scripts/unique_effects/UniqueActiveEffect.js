57a394b5523887bd5ca4b6eaa7f68c4d6aa23753
Michael Enion <caewok_2000@yahoo.com>
2024-12-21 11:25:23 -0800

    🐛 fix|ActiveEffect|Statuses not added
    When adding local effect, don't override existing statuses.
    
    Issue #46.

--- scripts/unique_effects/UniqueActiveEffect.js
+++ scripts/unique_effects/UniqueActiveEffect.js
@@ -85,9 +85,10 @@ export class UniqueActiveEffect extends AbstractUniqueEffect {
       doc.flags[MODULE_ID][FLAGS.UNIQUE_EFFECT.IS_LOCAL] = true;
       foundry.utils.mergeObject(doc, data);
       // Force display of the status icon
+      doc.statuses ??= [];
       if ( token.document.disposition !== CONST.TOKEN_DISPOSITIONS.SECRET
         && effect.img
-        && effect.displayStatusIcon ) doc.statuses = [effect.img];
+        && effect.displayStatusIcon ) doc.statuses.push(effect.img);
 
       // Remove the _id b/c the property might be locked. See PR #44.
       // Have to define it manually b/c for local docs, foundry will not assign random id. See #46.
